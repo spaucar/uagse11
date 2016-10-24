8451bbb6a892bbc2e0d626c2b8fdb211026f1960
S I D N E Y PA U C A R U A G <sidneypaucar@uagateway.org>
2016-10-24 13:05:03 +0000

    initial commit

---
+++ practice/practiceOne.java
@@ -0,0 +1,6 @@
+public class practiceOne{
+    public static void main(String[] args)
+    {
+        System.out.println("It works!");
+    }
+}
---
+++ unit_0_summer/helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("This Summer was the best...");
+        System.out.println("and by best I mean i made alot of money ^.^ and gained a couple of extra pounds here and there;)");
+        System.out.println("Its a shame though, most of my money was spent on food and transportation :/");
+    }
+}
